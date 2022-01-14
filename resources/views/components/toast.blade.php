b49fa72cd0242abdd032fedc60a4a957af2897ab
rabbitdev <rabbitdev1223@gmail.com>
2022-01-14 21:49:34 +0800

    update

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,4 +1,4 @@
-<div class="toast-container position-fixed top-0 end-0 p-3">
+<div class="toast-container position-fixed top-0  end-0 p-3" style="z-index:10000">
     <div id="successToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header bg-primary text-light">
             <h5 class="my-0">Success</h5>
