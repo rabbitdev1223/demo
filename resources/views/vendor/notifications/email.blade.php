d0a484119e82660e8da38c79d58b86039328d041
rabbitdev <rabbitdev1223@gmail.com>
2022-01-20 16:51:53 +0800

    create customized verfication email template

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,17 @@
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
