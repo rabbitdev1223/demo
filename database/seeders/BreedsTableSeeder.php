e37a1e7b37d364f684dfeb7da909303f7300068d
rabbitdev <rabbitdev1223@gmail.com>
2022-01-24 21:31:06 +0800

    add tableseeder

--- database/seeders/BreedsTableSeeder.php
+++ database/seeders/BreedsTableSeeder.php
@@ -28,5 +28,27 @@ class BreedsTableSeeder extends Seeder
         \DB::table('breeds')->insert(['name' => 'Cacatua Bianco']);
         \DB::table('breeds')->insert(['name' => 'Cacatua Crestagialla']);
         \DB::table('breeds')->insert(['name' => 'Cacatua delle Tanimbar']);
+        \DB::table('breeds')->insert(['name' => 'Caicco']);
+        \DB::table('breeds')->insert(['name' => 'Calopsite']);
+        \DB::table('breeds')->insert(['name' => 'Cenerino']);
+        \DB::table('breeds')->insert(['name' => 'Cocorita']);
+        \DB::table('breeds')->insert(['name' => 'Conuro del Sole']);
+        \DB::table('breeds')->insert(['name' => 'Conuro Guance Verdi']);
+        \DB::table('breeds')->insert(['name' => 'Ecletto']);
+        \DB::table('breeds')->insert(['name' => 'Inseparabile Alinere']);
+        \DB::table('breeds')->insert(['name' => 'Inseparabile di Fischer']);
+        \DB::table('breeds')->insert(['name' => 'Inseparabile Facciarosa']);
+        \DB::table('breeds')->insert(['name' => 'Inseparabile Mascherato']);
+        \DB::table('breeds')->insert(['name' => 'Kakariki Fronte Rossa']);
+        \DB::table('breeds')->insert(['name' => 'Lori Capinero']);
+        \DB::table('breeds')->insert(['name' => 'Lori Rosso']);
+        \DB::table('breeds')->insert(['name' => 'Lorichetto Arcobaleno']);
+        \DB::table('breeds')->insert(['name' => 'Pappagallo Alibronzate']);
+        \DB::table('breeds')->insert(['name' => 'Pappagallo Testablu']);
+        \DB::table('breeds')->insert(['name' => 'Parrocchetto dal Collare']);
+        \DB::table('breeds')->insert(['name' => 'Parrocchetto di Bourke']);
+        \DB::table('breeds')->insert(['name' => 'Parrocchetto di Derby']);
+        \DB::table('breeds')->insert(['name' => 'Parrocchetto di Lesson']);
+        \DB::table('breeds')->insert(['name' => 'Parrocchetto Monaco']);        
     }
 }
