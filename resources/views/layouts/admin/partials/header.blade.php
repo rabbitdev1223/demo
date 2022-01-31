be73270bb523f167ef8c4a3370c4b714fbfaec90
YaroslavNovikov1223 <88393684+YaroslavNovikov1223@users.noreply.github.com>
2022-02-01 03:22:15 +0800

    add image

--- resources/views/layouts/admin/partials/header.blade.php
+++ resources/views/layouts/admin/partials/header.blade.php
@@ -1,8 +1,7 @@
 <div class="page-main-header">
   <div class="main-header-right row m-0">
     <div class="main-header-left">
-      <div class="logo-wrapper"><a href="{{ route('index') }}"><img class="img-fluid" src="{{asset('assets/images/logo/logo.png')}}" alt=""></a></div>
-      <div class="dark-logo-wrapper"><a href="{{ route('index') }}"><img class="img-fluid" src="{{asset('assets/images/logo/dark-logo.png')}}" alt=""></a></div>
+      <div class="logo-wrapper"><a href="{{ route('index') }}"><img class="img-fluid" src="{{asset('assets/images') . '/logo-gestionale-100px.jpg' }}" alt=""></a></div>
       <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle">    </i></div>
     </div>
     <!-- <div class="left-menu-header col">
